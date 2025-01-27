51a298287b1fce1fbc136deaae048663102ef34d
sethikari <160271195+sethikari@users.noreply.github.com>
2025-01-27 17:21:36 -0500

    movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 movement;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (rb == null)
+        {
+            Debug.LogError("No Rigidbody2D found on the player object!");
+        }
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.magnitude > 1)
+        {
+            movement.Normalize();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
+    }
+}
