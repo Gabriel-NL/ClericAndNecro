0cf2233973a10525bafc60ff1a43d7b3818213b0
sethikari <160271195+sethikari@users.noreply.github.com>
2025-02-07 16:33:49 -0500

    added simple health system

---
+++ Assets/Scripts/Main/Player/HealthSystem.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    public Image[] hearts; // Assign heart images in the Inspector
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHealthUI();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died!");
+        gameObject.SetActive(false);
+    }
+
+    void UpdateHealthUI()
+    {
+        // Loop through hearts and enable/disable them based on health
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                hearts[i].enabled = true; // Show heart
+            }
+            else
+            {
+                hearts[i].enabled = false; // Hide heart
+            }
+        }
+    }
+}
