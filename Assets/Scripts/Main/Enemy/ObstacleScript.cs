d7d49f7f31be84d8ea24dceaa57ea5a7ef61dcae
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2025-04-02 15:36:44 -0400

    Fixed break state

---
+++ Assets/Scripts/Main/Enemy/ObstacleScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    // Start is called before the first frame update\
+
+    public BreakStateController break_status_script;
+    public int health=10;
+    void Start()
+    {
+        
+    }
+
+    public void ObstacleDamaged(int dmg){
+        
+    }
+}
